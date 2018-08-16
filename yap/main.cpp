e1409567d95d2f5c6380627a3d57b14e6b8e255f
chrisantus0816 <chrisantus@naver.com>
2018-08-16 12:14:24 +0900

    Initial Commit

---
+++ yap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  yap
+//
+//  Created by 이원준 on 2018. 8. 16..
+//  Copyright © 2018년 LEEWONJUN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
